61b19a15380baa3359e333187b245c23ff7a76f0
DonaldKisaka <kisakadonald1@gmail.com>
2025-04-15 09:16:30 +0300

    Final commit

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import React from "react";
 import { navLinks } from "@/constants";
 import MyButton from "@/components/ui/button";
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 const NavLinks = React.memo(() => (
   <nav className="flex flex-wrap items-center justify-center gap-12">
@@ -23,7 +25,16 @@ const NavLinks = React.memo(() => (
 NavLinks.displayName = "NavLinks";
 
 const Nav = () => {
-  return (
+
+  const [isOpen, setIsopen] = useState(false);
+  
+  const toggleMenu = () => {
+    setIsopen(!isOpen);
+  };
+
+
+  return ( 
+    // Desktop Navigation
     <div className="flex flex-col flex-1">
       <header className="border-b border-gray-200">
         <div className="container mx-auto flex h-16 items-center justify-between px-4">
@@ -44,10 +55,38 @@ const Nav = () => {
             <NavLinks />
           </div>
 
+          <div className="hidden max-md:block">
+            {
+              isOpen ? (<X width={30} height={30} onClick={toggleMenu} className="fixed right-12 top-12 cursor-pointer z-50 transition-all duration-300 hover:text-[#a3704f]"/>)
+               : (
+                <Menu width={30} height={30} onClick={toggleMenu} className="cursor-pointer hover:text-[#a3704f]" />
+               )
+            }
+          </div>
+          {
+            isOpen && (
+              <div className={`top-0 right-0 w-full bg-white shadow-md z-40 fixed h-full ${isOpen ? "flex" : "hidden"}`}>
+               <ul className="flex flex-col items-center justify-center absolute gap-8 top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]">
+                {navLinks.map(link => (
+                  <li key={link.label} className="w-full text-center py-6 border-gray-200">
+                    <Link href={link.href} className="block w-full font-bold text-gray-600 hover:text-[#a3704f] transition-colors duration-300 cursor-pointer no-underline" onClick={toggleMenu}>
+                      {link.label}
+                    </Link>
+                  </li>
+                ))}
+
+               </ul>
+
+              </div>
+            )
+          }
+
+          
+
           <div className="hidden w-full md:w-auto md:flex justify-center md:justify-end">
             <Link href="/contact">
               <MyButton size="sm" variant="default">
-                Contact Us
+                Contact Us 
               </MyButton>
             </Link>
           </div>
--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -16,18 +16,18 @@ const Pricing = () => {
   return (
     
       <section id="pricing" className="px-4 py-12 md:px-16">
-        <h2 className="text-3xl font-bold text-center mb-10">
+        <h2 className="text-2xl md:text-3xl font-bold text-center mb-8 md:mb-10">
             Pricing List
         </h2>
 
         {/* Tabs */}
-        <div className="flex justify-center space-x-4 mb-8">
+        <div className="flex flex-wrap justify-center max-md:gap-4 space-x-2 md:space-x-4 mb-6 md:mb-8">
             {
                 tabs.map(tab => (
                     <button 
                       key={tab} 
                       onClick={() => setActiveTab(tab as PricingKey)}
-                      className={`px-5 py-2 rounded-full border transition-all duration-300 ${activeTab === tab ?
+                      className={`px-4 py-2 text-sm md:text-base rounded-full border transition-all duration-300 ${activeTab === tab ?
                         "bg-[#a3704f] text-white"
                         : "border-gray-300 text-gray-700 hover:bg-gray-100"
                       }`}
@@ -38,19 +38,19 @@ const Pricing = () => {
         </div>
 
         {/* Pricing Details */}
-        <div className="border rounded-lg p-6 shadow-sm">
-            <div className="flex justify-between items-center border-b pb-4 mb-4">
-               <p className="text-4xl font-bold text-[#a3704f]">{current.price}</p>
+        <div className="border rounded-lg p-4 md:p-6 shadow-sm">
+            <div className="flex flex-col md:flex-row justify-between items-start md:items-center border-b pb-4 mb-4 space-y-4 md:space-y-0">
+               <p className="text-3xl md:text-4xl font-bold text-[#a3704f]">{current.price}</p>
                <MyButton size="sm">
                 BOOK SCHEDULE
                </MyButton>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-[#7c5c41]">
+            <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-4 text-[#7c5c41]">
                 {current.features.map((feature, index) => (
-                    <div key={index} className="flex items-start space-x-2 text-sm">
+                    <div key={index} className="flex items-start space-x-2 text-sm md:text-base">
                         <span className="mt-1">•</span>
-                        <p className="text-md font-semibold">{feature}</p>
+                        <p className="text-sm md:text-md font-semibold">{feature}</p>
                     </div>
                 ))}
             </div>
